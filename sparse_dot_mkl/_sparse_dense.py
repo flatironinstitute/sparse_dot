ec839f13b1d53b581b707006e3462da5b37d0205
asistradition <asistradition@users.noreply.github.com>
2020-01-30 12:03:03 -0500

    Dense * sparse

--- sparse_dot_mkl/_sparse_dense.py
+++ sparse_dot_mkl/_sparse_dense.py
@@ -5,16 +5,17 @@ from sparse_dot_mkl._mkl_interface import (MKL, _sanity_check, _empty_output_che
                                            _destroy_mkl_handle, matrix_descr, RETURN_CODES)
 
 
-def _sparse_dense_matmul(matrix_a, matrix_b, double_precision, m, n, k, scalar=1., b_is_sparse=False):
+def _sparse_dense_matmul(matrix_a, matrix_b, double_precision, m, n, scalar=1., b_is_sparse=False):
 
     output_shape = (m, n)
 
     # Allocate an array for outputs and set functions and types for float or doubles
-    output_arr = np.zeros(output_shape, dtype=np.float64 if double_precision else np.float32)
     output_ctype = _ctypes.c_double if double_precision else _ctypes.c_float
     func = MKL._mkl_sparse_d_mm if double_precision else MKL._mkl_sparse_s_mm
 
+    # matrix_a is an mkl handle and matrix_b is dense
     if not b_is_sparse:
+        output_arr = np.zeros(output_shape, dtype=np.float64 if double_precision else np.float32)
         ret_val = func(10,
                        scalar,
                        matrix_a,
@@ -25,15 +26,37 @@ def _sparse_dense_matmul(matrix_a, matrix_b, double_precision, m, n, k, scalar=1
                        matrix_b.shape[1],
                        1.,
                        output_arr.ctypes.data_as(_ctypes.POINTER(output_ctype)),
-                       output_shape[0])
+                       output_shape[1])
 
         # Check return
         if ret_val != 0:
             err_msg = "{fn} returned {v} ({e})".format(fn=func.__name__, v=ret_val, e=RETURN_CODES[ret_val])
             raise ValueError(err_msg)
 
+    # matrix_a is dense and and matrix_b is a mkl handle
+    # calculate BT (dot) AT and return the transpose (which will be A dot B)
     elif b_is_sparse:
-        raise NotImplementedError
+        output_arr = np.zeros(output_shape, dtype=np.float64 if double_precision else np.float32).T
+        ret_val = func(11,
+                       scalar,
+                       matrix_b,
+                       matrix_descr(),
+                       102,
+                       matrix_a,
+                       output_shape[0],
+                       matrix_a.shape[1],
+                       1.,
+                       output_arr.ctypes.data_as(_ctypes.POINTER(output_ctype)),
+                       output_shape[1])
+
+        # Check return
+        if ret_val != 0:
+            err_msg = "{fn} returned {v} ({e})".format(fn=func.__name__, v=ret_val, e=RETURN_CODES[ret_val])
+            raise ValueError(err_msg)
+
+        output_arr = output_arr.T
+    else:
+        raise ValueError("Boolean switch error in _sparse_dense_matmul")
 
     return output_arr
 
@@ -53,11 +76,15 @@ def _sparse_dot_dense(matrix_a, matrix_b, cast=False, dprint=print, scalar=1.):
         mkl_a, a_dbl = _create_mkl_sparse(matrix_a)
         b_dbl = matrix_b.dtype == np.float64
         matrix_c = _sparse_dense_matmul(mkl_a, matrix_b,
-                                        a_dbl or b_dbl, matrix_a.shape[0], matrix_b.shape[1], matrix_a.shape[1],
-                                        scalar=scalar)
+                                        a_dbl or b_dbl, matrix_a.shape[0], matrix_b.shape[1],
+                                        scalar=scalar, b_is_sparse=False)
         _destroy_mkl_handle(mkl_a)
         return matrix_c
     elif _spsparse.issparse(matrix_b):
-        raise NotImplementedError
-
-
+        mkl_b, b_dbl = _create_mkl_sparse(matrix_b)
+        a_dbl = matrix_a.dtype == np.float64
+        matrix_c = _sparse_dense_matmul(matrix_a, mkl_b,
+                                        a_dbl or b_dbl, matrix_a.shape[0], matrix_b.shape[1],
+                                        scalar=scalar, b_is_sparse=True)
+        _destroy_mkl_handle(mkl_b)
+        return matrix_c
--- sparse_dot_mkl/tests/test_mkl.py
+++ sparse_dot_mkl/tests/test_mkl.py
@@ -309,26 +309,50 @@ class TestSparseDenseMultiplication(unittest.TestCase):
 
     def setUp(self):
         self.mat1 = MATRIX_1.copy()
-        self.mat2 = MATRIX_2.copy().A
+        self.mat2 = MATRIX_2.copy()
 
-    def test_float32(self):
-        d1, d2 = self.mat1.astype(np.float32), self.mat2.astype(np.float32)
+    def test_float32_b_sparse(self):
+        d1, d2 = self.mat1.A.astype(np.float32), self.mat2.astype(np.float32)
+        mat3_np = np.dot(d1, d2.A)
+
+        mat3 = dot_product_mkl(d1, d2)
+
+        npt.assert_array_almost_equal(mat3_np, mat3)
+
+    def test_float64_b_sparse(self):
+        d1, d2 = self.mat1.A, self.mat2
+        mat3_np = np.dot(d1, d2.A)
+
+        mat3 = dot_product_mkl(d1, d2)
+
+        npt.assert_array_almost_equal(mat3_np, mat3)
+
+    def test_float64_cast_b_sparse(self):
+        d1, d2 = self.mat1.A.astype(np.float32), self.mat2
+        mat3_np = np.dot(d1, d2.A)
+
+        mat3 = dot_product_mkl(d1, d2, cast=True)
+
+        npt.assert_array_almost_equal(mat3_np, mat3)
+
+    def test_float32_a_sparse(self):
+        d1, d2 = self.mat1.astype(np.float32), self.mat2.A.astype(np.float32)
         mat3_np = np.dot(d1.A, d2)
 
         mat3 = dot_product_mkl(d1, d2)
 
         npt.assert_array_almost_equal(mat3_np, mat3)
 
-    def test_float64(self):
-        d1, d2 = self.mat1, self.mat2
+    def test_float64_a_sparse(self):
+        d1, d2 = self.mat1, self.mat2.A
         mat3_np = np.dot(d1.A, d2)
 
         mat3 = dot_product_mkl(d1, d2)
 
         npt.assert_array_almost_equal(mat3_np, mat3)
 
-    def test_float64_cast(self):
-        d1, d2 = self.mat1.astype(np.float32), self.mat2
+    def test_float64_cast_a_sparse(self):
+        d1, d2 = self.mat1.astype(np.float32), self.mat2.A
         mat3_np = np.dot(d1.A, d2)
 
         mat3 = dot_product_mkl(d1, d2, cast=True)
