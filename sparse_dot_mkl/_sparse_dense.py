6d5e105ff7d1dabe01b22da70b3436a42a74736d
asistradition <asistradition@users.noreply.github.com>
2020-04-19 12:50:28 -0400

    v 0.3.3 simplified sparse dot dense logic

--- sparse_dot_mkl/_sparse_dense.py
+++ sparse_dot_mkl/_sparse_dense.py
@@ -6,101 +6,82 @@ import ctypes as _ctypes
 import scipy.sparse as _spsparse
 
 
-def _sparse_dense_matmul(matrix_a, matrix_b, scalar=1.):
+def _dense_sparse_matmul(matrix_a, matrix_b, scalar=1.):
     """
-    Multiply together a dense and a sparse matrix
-    mkl_sparse_?_mm requires the left (A) matrix to be sparse and the right (B) matrix to be dense
-    If B is sparse, calculate BT (dot) AT; the transpose of this product is equal to A (dot) B
-    This requires conversion of the sparse matrix to CSR format for some dense arrays;
-    If A is sparse, it must be CSR iff B is column-major
-    If B is sparse, it must be CSR iff B is row-major
+    Calculate BT (dot) AT and then transpose this product. It will be equal to A (dot) B
 
     :param matrix_a: Left (A) matrix
-    :type matrix_a: np.ndarray, sp.spmatrix.csr, sp.spmatrix.csc
+    :type matrix_a: np.ndarray
     :param matrix_b: Right (B) matrix
-    :type matrix_b: np.ndarray, sp.spmatrix.csr, sp.spmatrix.csc
-    :param scalar: A value to multiply the result matrix by. Defaults to 1.
+    :type matrix_b: sp.spmatrix.csr, sp.spmatrix.csc
     :param scalar: float
     :return: A (dot) B as a dense array in either column-major or row-major format
     :rtype: np.ndarray
     """
-    output_shape = (matrix_a.shape[0], matrix_b.shape[1])
-    b_is_sparse = _spsparse.isspmatrix(matrix_b)
+
+    matrix_a = matrix_a.T
+    return _sparse_dense_matmul(matrix_b, matrix_a, scalar=scalar, transpose=True).T
+
+
+def _sparse_dense_matmul(matrix_a, matrix_b, scalar=1., transpose=False):
+    """
+    Multiply together a sparse and a dense matrix
+    mkl_sparse_?_mm requires the left (A) matrix to be sparse and the right (B) matrix to be dense
+    If B is sparse, calculate BT (dot) AT; the transpose of this product is equal to A (dot) B
+    This requires conversion of the sparse matrix to CSR format for some dense arrays;
+    If A must be CSR iff B is column-major
+
+    :param matrix_a: Left (A) matrix
+    :type matrix_a: sp.spmatrix.csr, sp.spmatrix.csc
+    :param matrix_b: Right (B) matrix
+    :type matrix_b: np.ndarray
+    :param scalar: A value to multiply the result matrix by. Defaults to 1.
+    :type scalar: float
+    :param transpose: Return AT (dot) B instead of A (dot) B.
+    :type transpose: bool
+    :return: A (dot) B as a dense array in either column-major or row-major format
+    :rtype: np.ndarray
+    """
+
+    output_shape = (matrix_a.shape[1] if transpose else matrix_a.shape[0], matrix_b.shape[1])
 
     # Prep MKL handles and check that matrixes are compatible types
     # MKL requires the sparse matrix to be CSR if the dense matrix is column-major (F)
-    if not b_is_sparse:
-        mkl_a, dbl = _create_mkl_sparse(matrix_a)
 
-        layout_b, ld_b = _get_numpy_layout(matrix_b)
+    mkl_a, dbl = _create_mkl_sparse(matrix_a)
+    layout_b, ld_b = _get_numpy_layout(matrix_b)
 
-        if layout_b == LAYOUT_CODE_F and not _spsparse.isspmatrix_csr(matrix_a):
-            mkl_a = _convert_to_csr(mkl_a, destroy_original=True)
-    else:
-        mkl_b, dbl = _create_mkl_sparse(matrix_b)
+    # MKL requires CSR format if the dense array is column-major
+    if layout_b == LAYOUT_CODE_F and not _spsparse.isspmatrix_csr(matrix_a):
+        mkl_a = _convert_to_csr(mkl_a, destroy_original=True)
 
-        matrix_a = matrix_a.T
-        layout_a, ld_a = _get_numpy_layout(matrix_a)
-
-        if layout_a == LAYOUT_CODE_F and not _spsparse.isspmatrix_csr(matrix_b):
-            mkl_b = _convert_to_csr(mkl_b, destroy_original=True)
-
-    # Allocate an array for outputs and set functions and types for float or doubles
+    # Set functions and types for float or doubles
     output_ctype = _ctypes.c_double if dbl else _ctypes.c_float
+    output_dtype = np.float64 if dbl else np.float32
     func = MKL._mkl_sparse_d_mm if dbl else MKL._mkl_sparse_s_mm
 
-    # matrix_a is an mkl handle and matrix_b is dense
-    if not b_is_sparse:
+    # Allocate an output array
+    output_arr = np.zeros(output_shape, dtype=output_dtype, order="C" if layout_b == LAYOUT_CODE_C else "F")
+    _, output_ld = _get_numpy_layout(output_arr)
 
-        output_arr = np.zeros(output_shape, dtype=np.float64 if dbl else np.float32,
-                              order="C" if layout_b == LAYOUT_CODE_C else "F")
-        output_ld = output_shape[1] if layout_b == LAYOUT_CODE_C else output_shape[0]
+    ret_val = func(11 if transpose else 10,
+                   scalar,
+                   mkl_a,
+                   matrix_descr(),
+                   layout_b,
+                   matrix_b,
+                   output_shape[1],
+                   ld_b,
+                   1.,
+                   output_arr.ctypes.data_as(_ctypes.POINTER(output_ctype)),
+                   output_ld)
 
-        ret_val = func(10,
-                       scalar,
-                       mkl_a,
-                       matrix_descr(),
-                       layout_b,
-                       matrix_b,
-                       output_shape[1],
-                       ld_b,
-                       1.,
-                       output_arr.ctypes.data_as(_ctypes.POINTER(output_ctype)),
-                       output_ld)
+    # Check return
+    if ret_val != 0:
+        err_msg = "{fn} returned {v} ({e})".format(fn=func.__name__, v=ret_val, e=RETURN_CODES[ret_val])
+        raise ValueError(err_msg)
 
-        # Check return
-        if ret_val != 0:
-            err_msg = "{fn} returned {v} ({e})".format(fn=func.__name__, v=ret_val, e=RETURN_CODES[ret_val])
-            raise ValueError(err_msg)
-
-        _destroy_mkl_handle(mkl_a)
-
-    # matrix_a is dense and and matrix_b is a mkl handle
-    # calculate BT (dot) AT and return the transpose (which will be A dot B)
-    elif b_is_sparse:
-        output_arr = np.zeros(output_shape, dtype=np.float64 if dbl else np.float32,
-                              order="F" if layout_a == LAYOUT_CODE_C else "C").T
-        output_ld = output_shape[0] if layout_a == LAYOUT_CODE_C else output_shape[1]
-
-        ret_val = func(11,
-                       scalar,
-                       mkl_b,
-                       matrix_descr(),
-                       layout_a,
-                       matrix_a,
-                       output_shape[0],
-                       ld_a,
-                       1.,
-                       output_arr.ctypes.data_as(_ctypes.POINTER(output_ctype)),
-                       output_ld)
-
-        # Check return
-        if ret_val != 0:
-            err_msg = "{fn} returned {v} ({e})".format(fn=func.__name__, v=ret_val, e=RETURN_CODES[ret_val])
-            raise ValueError(err_msg)
-
-        output_arr = output_arr.T
-        _destroy_mkl_handle(mkl_b)
+    _destroy_mkl_handle(mkl_a)
 
     return output_arr
 
@@ -126,5 +107,7 @@ def _sparse_dot_dense(matrix_a, matrix_b, cast=False, dprint=print, scalar=1.):
 
     if sum([_spsparse.isspmatrix(matrix_a), _spsparse.isspmatrix(matrix_b)]) != 1:
         raise ValueError("_sparse_dot_dense takes one sparse and one dense array")
-
-    return _sparse_dense_matmul(matrix_a, matrix_b, scalar=scalar)
+    elif _spsparse.isspmatrix(matrix_a):
+        return _sparse_dense_matmul(matrix_a, matrix_b, scalar=scalar)
+    elif _spsparse.isspmatrix(matrix_b):
+        return _dense_sparse_matmul(matrix_a, matrix_b, scalar=scalar)
