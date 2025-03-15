d54deb8fbaf63ce165dd9a8ebe420f618e92a971
asistradition <cj59@nyu.edu>
2025-03-15 11:39:59 -0400

    Skip if github actions, keep for local

--- sparse_dot_mkl/tests/test_iss.py
+++ sparse_dot_mkl/tests/test_iss.py
@@ -49,7 +49,7 @@ class TestSparseSolverCG(unittest.TestCase):
         self.mat2 = test_rhs.copy()
         self.mat3 = test_expected_solution.copy()
 
-    @unittest.skipIf(os.getenv("GITHUB_ACTIONS", None) is None, 'NOT ON CI')
+    @unittest.skipIf(os.getenv("GITHUB_ACTIONS", None) is not None, 'NOT ON CI')
     def test_cg_solver_square_perfect(self):
         mat3 = np.linalg.lstsq(self.mat1.toarray(), test_rhs, rcond=None)[0]
 
@@ -61,7 +61,7 @@ class TestSparseSolverCG(unittest.TestCase):
         npt.assert_array_equal(test_rhs, self.mat2)
         npt.assert_array_almost_equal(x, mat3)
 
-    @unittest.skipIf(os.getenv("GITHUB_ACTIONS", None) is None, 'NOT ON CI')
+    @unittest.skipIf(os.getenv("GITHUB_ACTIONS", None) is not None, 'NOT ON CI')
     def test_cg_wrapper_square_perfect(self):
 
         mat3 = np.linalg.lstsq(self.mat1.toarray(), test_rhs, rcond=None)[0]
